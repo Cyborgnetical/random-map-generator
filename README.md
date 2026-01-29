6c62fe40cc03e7d10f7cbe310efe775e474e1094
Cyborgnetical <140975796+Cyborgnetical@users.noreply.github.com>
2026-01-29 11:13:48 -0800

    I got simple terrain generation working. Along with biomes.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Random Map Generator
+
+A simple map generator to make a random map each time.
+Currently it only makes a simple plans with rivers and small hills
+
+## Plans:
+
+- Add biomes
+- Add Modifiers
+- Add a simple ui to change properties
+- Generate landmarks such as trees, rocks, flowers, etc
---
+++ src/main.js
@@ -0,0 +1,111 @@
+// grid stuff
+let grid = []
+let gridSize = [160,160]
+let gridSquareSize = 5
+
+/**
+ * @type {HTMLCanvasElement}
+ */
+let canvas = document.getElementById("mapCanvas");
+/**
+ * @type {CanvasRenderingContext2D}
+ */
+let ctx = canvas.getContext("2d");
+
+
+class Cube{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+
+    // perlin is werid, it needs decimals
+    this.decimalX = x/gridSize[0]
+    this.decimalY = y/gridSize[1]
+    let value = Math.floor(perlin.get(this.decimalX,this.decimalY)*22)
+    if(value < 0){
+      value = -value
+    }
+    this.value = value
+    console.log(value)
+  }
+}
+
+perlin.seed()
+let colors = [
+  "blue",
+  "yellow",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "grey",
+  "grey",
+  "grey",
+  "lightgrey",
+]
+
+let biomes =  {
+  grasslands:[ "blue",
+  "yellow",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "grey",
+  "grey",
+  "grey",
+  "lightgrey"],
+  ocean:[
+    "darkblue",
+    "darkblue",
+    "darkblue",
+    "darkblue",
+    "darkblue",
+    "blue",
+    "blue",
+    "blue",
+    "blue",
+    "blue",
+    "blue",
+    "blue",
+    "blue",
+    "blue",
+    "cyan"
+  ],
+  hills:[
+  "blue",
+  "yellow",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "blue",
+  "grey",
+  "grey",
+  "grey",
+  "lightgrey"
+  ]
+}
+for(let i=0;i<gridSize[0];i++){
+  for(let o=0;o<gridSize[1];o++){
+    let cube = new Cube(i*gridSquareSize,o*gridSquareSize)
+    ctx.beginPath()
+    ctx.rect(i*gridSquareSize,o*gridSquareSize,gridSquareSize,gridSquareSize)
+    ctx.fillStyle = biomes.ocean[cube.value]
+    ctx.fill()
+  }
+}
